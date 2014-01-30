524a4893013378ca526039dac5914645e46b8d23
Gísli Kristjánsson <gislik@hamstur.is>
2014-01-30 18:14:06 +0000

    Access stdin/stdout through Data.ByteString

--- src/Network/Zmcat.hs
+++ src/Network/Zmcat.hs
@@ -1,12 +1,13 @@
 
 module Network.Zmcat where
 
+import Prelude hiding (getLine, drop, length)
 import System.ZMQ3
 import Control.Monad (forever)
-import qualified Data.ByteString as BS
+import Data.Monoid ((<>))
+import Data.ByteString
 import qualified Data.ByteString.Char8 as C
-import Data.ByteString.Char8 (pack, unpack)
-import System.IO
+import System.IO (stdout, hFlush)
 
 runCtx :: SocketType a1 => a1 -> (Socket a1 -> IO a) -> IO a
 runCtx t blk = withContext $ \ctx -> do
@@ -17,16 +18,16 @@ pub :: String -> String -> IO a
 pub uri k = runCtx Pub $ \skt -> do
         bind skt uri
         forever $ do
-            pkt <- hGetLine stdin
-            send skt [] (pack $ k ++ pkt)
+            pkt <- getLine 
+            send skt [] (C.pack k <> pkt)
 
-sub :: String -> BS.ByteString -> IO a
+sub :: String -> ByteString -> IO a
 sub uri k = runCtx Sub $ \skt -> do
         subscribe skt k
         connect skt uri
         forever $ do
             line <- receive skt
-            putStrLn $ drop (BS.length k) (unpack line)
+            C.putStrLn $ drop (length k) line
             hFlush stdout
                 
 pull :: String -> IO a
@@ -34,32 +35,32 @@ pull uri = runCtx Pull $ \skt -> do
         bind skt uri
         forever $ do
             line <- receive skt
-            putStrLn $ unpack line
+            C.putStrLn line
             hFlush stdout
 
 push :: String -> IO a
 push uri = runCtx Push $ \skt -> do
         connect skt uri
         forever $ do
-            pkt <- hGetLine stdin
-            send skt [] (pack pkt)
+            pkt <- getLine 
+            send skt [] pkt
 
 rep :: String -> IO a
 rep uri = runCtx Rep $ \skt -> do
     bind skt uri
     forever $ do
         line <- receive skt
-        putStrLn $ unpack line
+        C.putStrLn line
         hFlush stdout
-        pkt <- hGetLine stdin
-        send skt [] (pack pkt)
+        pkt <- getLine 
+        send skt [] pkt
 
 req :: String -> IO a
 req uri = runCtx Req $ \skt -> do
     connect skt uri
     forever $ do
-        pkt <- hGetLine stdin
-        send skt [] (pack pkt)
+        pkt <- getLine 
+        send skt [] pkt
         lines' <- receiveMulti skt
         mapM_ C.putStrLn lines'
         hFlush stdout
