0a04d2f76d25b451e245ea82a3685910612832ce
Gísli Kristjánsson <gislik@hamstur.is>
2014-01-30 19:21:10 +0000

    Initial commit for the eof-handler feature

--- bin/Zmcat.hs
+++ bin/Zmcat.hs
@@ -3,7 +3,10 @@
 module Main where
 
 import System.Exit (exitFailure)
+import Control.Exception (handleJust)
+import Control.Monad (guard)
 import System.IO
+import System.IO.Error (isEOFError)
 import System.Environment (getArgs)
 import qualified Data.ByteString.Char8 as C
 
@@ -17,7 +20,7 @@ main = do
 		["sub", uri, k] 	-> sub uri $ C.pack k
 		["pub", uri]		-> pub uri ""
 		["sub", uri] 		-> sub uri ""
-		["push", uri] 		-> push uri
+		["push", uri] 		-> handleJust (guard . isEOFError) return $ push uri
 		["pull", uri] 		-> pull uri
 		["req", uri] 		-> req uri
 		["rep", uri] 		-> rep uri
